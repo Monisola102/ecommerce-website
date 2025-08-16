80d84960380c5680e8e6bf46be147b4c57cfe867
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-16 21:58:37 +0100

    shopping button fixed

--- component/order-success.tsx
+++ component/order-success.tsx
@@ -18,14 +18,14 @@ export default function OrderSuccessPage() {
         <div className="flex flex-col gap-4">
           <Link
             href="/account/orders"
-            className="bg-gradient-to-r from-red-400 via-green-400 to-gray-400 text-white px-6 py-2 rounded hover:opacity-90"
+            className="bg-gradient-to-r from-red-400 via-green-200 to-gray-400 text-black px-6 py-2 rounded hover:opacity-90"
           >
             View My Orders
           </Link>
 
           <Link
             href="/"
-            className="bg-gradient-to-r from-red-400 via-green-400 to-gray-400 text-black px-6 py-2 rounded hover:opacity-90"
+            className="bg-gradient-to-r from-red-400 via-green-200 to-gray-400 text-black px-6 py-2 rounded hover:opacity-90"
           >
             Continue Shopping
           </Link>
