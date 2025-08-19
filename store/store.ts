c39b52cd9872f4a2e54bdb2031cc3b5d2d7dbbc0
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-19 13:16:20 +0100

    brand api middleware added to the store

--- store/store.ts
+++ store/store.ts
@@ -3,11 +3,13 @@ import { cartApi } from "./Features/cart/cart-api";
 import { likeApi } from "./Features/like/like-api";
 import { productsApi } from "./products/product-api";
 import { orderApi } from "./Features/order/order-api";
+import { brandApi } from "./Features/brands/brand-api";
 import likeReducer from "./Features/like/like-slice";
 import authReducer from "./Features/auth/auth-slice";
 import productReducer from "./products/product-slice";
 import cartReducer from "./Features/cart/cart-slice";
 import orderReducer from "./Features/order/order-slice";
+import brandReducer from "./Features/brands/brand-slice";
 
 import { configureStore } from "@reduxjs/toolkit";
 
@@ -18,16 +20,18 @@ cart:cartReducer,
 like:likeReducer,
 product:productReducer,
 order:orderReducer,
+brand:brandReducer,
     [authApi.reducerPath]: authApi.reducer,
     [cartApi.reducerPath]: cartApi.reducer,
     [likeApi.reducerPath]: likeApi.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
     [orderApi.reducerPath]: orderApi.reducer,
+    [brandApi.reducerPath]: brandApi.reducer,
 
   },
 
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(authApi.middleware, cartApi.middleware, likeApi.middleware, productsApi.middleware, orderApi.middleware);
+    return getDefaultMiddleware().concat(authApi.middleware, cartApi.middleware, likeApi.middleware, productsApi.middleware, orderApi.middleware, brandApi.middleware);
   },
 });
 
