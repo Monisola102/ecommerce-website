872a58055cde8149c4ca91ff195976e08c3d861b
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-20 19:19:00 +0100

    brand page fixed

--- app/(no-footer)/Brands/[brand]/page.tsx
+++ app/(no-footer)/Brands/[brand]/page.tsx
@@ -1,5 +1,9 @@
 import BrandProductsPage from "@/component/brand";
 
-export default function BrandPage({ params }: { params: { brand: string } }) {
+export default function BrandPage({
+  params,
+}: {
+  params: { brand: string };
+}) {
   return <BrandProductsPage brand={params.brand} />;
 }
