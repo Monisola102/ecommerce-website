d4a45212ca5f255a854fa0f3a669813a163f9846
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-20 10:37:10 +0100

    interface fixed

--- component/trendCard.tsx
+++ component/trendCard.tsx
@@ -25,7 +25,10 @@ interface SizeType {
 interface trendInterface {
   _id: string;
   image: string;
-  brand: string;
+  brand: {
+  _id: string;
+  name: string;
+};
   name: string;
   price: number;
   sizes: SizeType[];
@@ -120,7 +123,7 @@ const handleAddToCart = async () => {
       </div>
 
       <div className="mt-2">
-        <p className="text-gray-400 text-[10px]">{trend.brand}</p>
+        <p className="text-gray-400 text-[10px]">{trend.brand?.name}</p>
         <p className="text-black text-[12px]">{trend.name}</p>
 
         <div className="flex gap-2 mt-1">
