db8fe08aa4a2b1b88841bf477b36ee709cb047f0
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-22 07:34:21 +0100

    sort and filter added

--- app/(no-footer)/Brands/[brand]/page.tsx
+++
@@ -1,9 +0,0 @@
-import BrandProductsPage from "@/component/brand";
-
-export default function BrandPage({
-  params,
-}: {
-  params: { brand: string };
-}) {
-  return <BrandProductsPage brand={params.brand} />;
-}
--- store/products/product-api.ts
+++ store/products/product-api.ts
@@ -41,20 +41,44 @@ export const productsApi = createApi({
   endpoints: (builder) => ({
     getProducts: builder.query<
       ProductResponse,
-      { page?: number; limit?: number; search?: string; category?: string; sort?: string; brand?: string; }
+      {
+        page?: number;
+        limit?: number;
+        search?: string;
+        category?: string;
+        minPrice?: number;
+        maxPrice?: number;
+        sort?: string;
+        brand?: string;
+        order?: "asc" | "desc";
+      }
     >({
-      query: ({ page = 1, limit = 10, search = "", category = "", sort = "" }) => {
+      query: ({
+        page = 1,
+        limit = 10,
+        search = "",
+        category = "",
+        brand = "",
+        minPrice,
+        maxPrice,
+        sort = "createdAt",
+        order = "desc",
+      }) => {
         const params = new URLSearchParams();
         params.set("page", page.toString());
         params.set("limit", limit.toString());
         if (search) params.set("search", search);
         if (category) params.set("category", category);
         if (sort) params.set("sort", sort);
-         if (brand) params.set("brand", brand);
+        if (brand) params.set("brand", brand);
+        if (minPrice !== undefined) params.set("minPrice", minPrice.toString());
+        if (maxPrice !== undefined) params.set("maxPrice", maxPrice.toString());
+        if (order) params.set("order", order);
+
         return `?${params.toString()}`;
       },
     }),
-     getProductById: builder.query<SingleProductResponse, string>({
+    getProductById: builder.query<SingleProductResponse, string>({
       query: (id) => `/${id}`,
     }),
   }),
