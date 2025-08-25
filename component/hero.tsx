909f39050203aa94ebfaafc2baa6bfadd1d478b7
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-25 07:27:55 +0100

    paragraphs responsiveness

--- component/hero.tsx
+++ component/hero.tsx
@@ -4,11 +4,11 @@ export default function Hero() {
       <div className="h-[65vh] sm:h-[60vh] md:h-[65vh] lg:h-[80vh] xl:h-[85vh] 2xl:h-[90vh] bg-[url('/heroooo-bg.jpg')] bg-cover bg-center relative">
         <div className="h-full flex items-center px-4 sm:px-6 md:px-10 lg:px-20 text-white">
           <div className="mt-[50px] sm:mt-[70px] md:mt-[100px] w-full sm:w-[80%] md:w-[60%] lg:w-[50%] flex flex-col gap-y-2">
-            <h1 className="text-[30px] sm:text-[40px] md:text-[50px] lg:text-[60px] xl:text-[66.94px] font-bold tracking-[1px] sm:tracking-[1.5px] md:tracking-[2px] font-inter">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-wide font-inter">
               NEW COLLECTION
             </h1>
-            <p className="text-[18px] sm:text-[22px] md:text-[25px] lg:text-[28px] xl:text-[29.29px] font-bold">
-              SPRING/SUMMER2025
+            <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold">
+              SPRING / SUMMER 2025
             </p>
           </div>
         </div>
