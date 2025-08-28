a235a3afcc20c8cd06622817341a2501f2a09597
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-28 19:59:42 +0100

    image removedd

--- component/learn.tsx
+++ component/learn.tsx
@@ -5,14 +5,15 @@ export default function Learn() {
   return (
     <div className="container mb-[200px] sm:mb-[250px] lg:mb-[300px]">
       <div className="flex justify-start mt-[50px]">
-        <div className="
-          relative left-0 
-    p-6 sm:p-8 lg:p-10 
-    min-[635px]:left-auto min-[635px]:p-4   /* 👈 shift to edge & reduce padding */
+        <div
+          className="
+         relative left-0 p-6 sm:p-8 lg:p-10
+    min-[635px]:w-full min-[635px]:p-4
     bg-zinc-400 text-white 
     w-full sm:w-[600px] md:w-[700px] lg:w-[800px] 
     h-auto lg:h-[320px]
-        ">
+        "
+        >
           {/* Text Section */}
           <div className="w-full sm:w-[300px] flex flex-col">
             <h6 className="mb-5 font-bold text-[16px] sm:text-[18px] tracking-wider">
@@ -20,8 +21,8 @@ export default function Learn() {
             </h6>
             <p className="font-bold text-[14px] sm:text-[15px]">
               Try now, pay later. We want that you're really confident and happy
-              with your purchase - you have 30 days before we have to charge you!
-              Learn more about our policy.
+              with your purchase - you have 30 days before we have to charge
+              you! Learn more about our policy.
             </p>
           </div>
 
@@ -34,8 +35,9 @@ export default function Learn() {
           <div
             className="
               hidden sm:block absolute 
-              right-[-120px] md:right-[-230px] lg:right-[-300px]
-              top-10 sm:top-14 md:top-18 lg:top-22
+    right-[-120px] md:right-[-230px] lg:right-[-300px]
+    top-10 sm:top-14 md:top-18 lg:top-22
+    min-[635px]:hidden max-[763px]:hidden
             "
           >
             <Image
@@ -45,9 +47,9 @@ export default function Learn() {
               height={280}
               className="
                 w-[400px] h-[200px] 
-                sm:w-[500px] sm:h-[240px] 
-                md:w-[600px] md:h-[260px] 
-                lg:w-[700px] lg:h-[280px]
+      sm:w-[500px] sm:h-[240px] 
+      md:w-[600px] md:h-[260px] 
+      lg:w-[700px] lg:h-[280px]
               "
             />
           </div>
