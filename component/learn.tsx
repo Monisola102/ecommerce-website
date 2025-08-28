a98a580534bf60afdcfa4a99bbfcf7d53dca7d1a
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-28 20:57:29 +0100

    images removed

--- component/learn.tsx
+++ component/learn.tsx
@@ -5,11 +5,13 @@ export default function Learn() {
   return (
     <div className="container mb-[200px] sm:mb-[250px] lg:mb-[300px]">
       <div className="flex justify-start mt-[50px]">
-        <div className="
+        <div
+          className="
           relative left-0 p-6 sm:p-8 lg:p-10 bg-zinc-400 text-white 
           w-full sm:w-[600px] md:w-[700px] lg:w-[800px] 
           h-auto lg:h-[320px]
-        ">
+        "
+        >
           {/* Text Section */}
           <div className="w-full sm:w-[300px] flex flex-col">
             <h6 className="mb-5 font-bold text-[16px] sm:text-[18px] tracking-wider">
@@ -17,8 +19,8 @@ export default function Learn() {
             </h6>
             <p className="font-bold text-[14px] sm:text-[15px]">
               Try now, pay later. We want that you're really confident and happy
-              with your purchase - you have 30 days before we have to charge you!
-              Learn more about our policy.
+              with your purchase - you have 30 days before we have to charge
+              you! Learn more about our policy.
             </p>
           </div>
 
@@ -30,10 +32,8 @@ export default function Learn() {
           {/* Image Section */}
           <div
             className="
-            hidden sm:block absolute 
-    right-[-120px] md:right-[-230px] lg:right-[-300px]
-    top-10 sm:top-14 md:top-18 lg:top-22
-    max-[790px]:hidden 
+         hidden min-[801px]:block absolute 
+    right-[-300px] top-22
             "
           >
             <Image
@@ -43,9 +43,6 @@ export default function Learn() {
               height={280}
               className="
                 w-[400px] h-[200px] 
-      sm:w-[500px] sm:h-[240px] 
-      md:w-[600px] md:h-[260px] 
-      lg:w-[700px] lg:h-[280px]
               "
             />
           </div>
