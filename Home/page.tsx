c00d9b9396789e298cd6ae4d346592e3687d2ba2
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-30 18:17:37 +0100

    loader added

--- Home/page.tsx
+++ Home/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState, useEffect } from "react";
+import Loader from "@/component/loader";
 import Hero from "@/component/hero";
 import TrendComp from "@/component/Trends";
 import SpringComp from "@/component/springComp";
@@ -5,25 +8,30 @@ import Learn from "@/component/learn";
 import RecommendedComp from "@/component/Recommended";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000); // simulate API wake
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <Loader />;
+
   return (
     <>
       <Hero />
-       <div className="mb-8">
+      <div className="mb-8">
         <TrendComp />
       </div>
-
       <div className="mb-8">
         <SpringComp />
       </div>
-
       <div className="mb-8">
         <RecommendedComp />
       </div>
-
       <div className="mb-8">
         <Learn />
       </div>
     </>
   );
 }
-5
---
+++ component/loader.tsx
@@ -0,0 +1,8 @@
+// components/Loader.tsx
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="w-12 h-12 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  );
+}
