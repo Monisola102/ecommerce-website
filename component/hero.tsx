ac465b8c09f8912fc060a9a6e565cac19f2161fd
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-30 18:54:38 +0100

    hero height fixed

--- component/hero.tsx
+++ component/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="w-full relative h-[65vh] sm:h-[45vh] md:h-[60vh] lg:h-[80vh] xl:h-[85vh] 2xl:h-[90vh]">
+    <div className="w-full relative h-[45vh] sm:h-[45vh] md:h-[60vh] lg:h-[80vh] xl:h-[85vh] 2xl:h-[90vh]">
       {/* Hero background */}
       <Image
         src="/heroooo-bg.jpg"
@@ -14,7 +14,7 @@ export default function Hero() {
 
       {/* Content */}
       <div className="h-full flex items-center px-4 sm:px-6 md:px-10 lg:px-20 text-white">
-        <div className="mt-[50px] sm:mt-[70px] md:mt-[100px] w-full sm:w-[80%] md:w-[60%] lg:w-[50%] flex flex-col gap-y-2">
+        <div className="mt-[50px] sm:mt-[60px] md:mt-[100px] w-full sm:w-[80%] md:w-[60%] lg:w-[50%] flex flex-col gap-y-2">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-wide font-inter">
             NEW COLLECTION
           </h1>
